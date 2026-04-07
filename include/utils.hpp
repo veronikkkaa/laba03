94b69a1552e72e57b430d608b20232794a1f0599
veronikkkaa <verdmigarshina@edu.hse.ru>
2026-04-08 01:38:41 +0300

    я щас заплачу, почему в контестах все было хорошо..

--- include/utils.hpp
+++ include/utils.hpp
@@ -5,4 +5,4 @@
 std::string to_lower_copy(std::string s);
 bool is_builtin_function(const std::string& s);
 std::string number_to_string(double x);
-void ensure_finite(double x, const std:
+void ensure_finite(double x, const std::string& msg);
