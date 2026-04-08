50937dcb66b89f2b8b642d993367b38aef23a561
veronikkkaa <verdmigarshina@edu.hse.ru>
2026-04-08 11:01:39 +0300

    мя

--- include/utils.hpp
+++ include/utils.hpp
@@ -7,3 +7,5 @@ std::string to_lower_copy(const std::string& s);
 bool is_builtin_function(const std::string& name);
 
 std::string number_to_string(double value);
+
+void ensure_finite(double value, const std::string& message);
